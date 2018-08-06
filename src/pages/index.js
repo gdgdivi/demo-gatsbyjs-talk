afd61a3003531227920798c0461b92fc85603139
Vinícius Soares <squimer@gmail.com>
2018-08-05 23:24:13 -0300

    Add post list to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,53 @@
+import { Link, graphql } from 'gatsby'
+import get from 'lodash/get'
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/contact/">Contato</Link>
-  </Layout>
+const Post = ({ node }) => (
+  <div key={node.fields.slug}>
+    <h3>
+      <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+        {node.frontmatter.title}
+      </Link>
+    </h3>
+    <small>{node.frontmatter.date}</small>
+    <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+    <hr />
+  </div>
 )
 
+const IndexPage = ({ data }) => {
+  const posts = get(data, 'allMarkdownRemark.edges', [])
+
+  return (
+    <Layout>
+      {posts.map(({ node }) => 
+        <Post node={node} key={node.fields.slug} />
+      )}
+
+      <Link to="/contact/">Contato</Link>
+    </Layout>
+  )
+}
+
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD MMMM, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
